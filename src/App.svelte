1d1da77dbf0a0761fead584d745ff16879c93d4a
Buse <busenuraktilav@gmail.com>
2022-11-05 23:57:13 +0100

    initial commit - setup works

--- src/App.svelte
+++ src/App.svelte
@@ -21,6 +21,9 @@
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite! Test
   </p>
+  <p>
+    Hello World!
+  </p>
 
   <p class="read-the-docs">
     Click on the Vite and Svelte logos to learn more
