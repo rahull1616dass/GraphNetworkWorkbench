b2c125f7901a7dcdd3b4fc603293fdc4c8148ecc
Kerem Cömert <kerem.coemert@elektrobit.com>
2022-11-17 13:50:22 +0100

    Host miserables.json under public

--- src/lib/Plot/Plot.svelte
+++ src/lib/Plot/Plot.svelte
@@ -1,37 +1,11 @@
 <script lang="ts">
-    /*
-
-    import { Vega } from "svelte-vega"
-
-  
-    const data = {
-      table: [
-        { category: "A", amount: 28 },
-        { category: "B", amount: 55 },
-        { category: "C", amount: 43 },
-        { category: "D", amount: 91 },
-        { category: "E", amount: 81 },
-        { category: "F", amount: 53 },
-        { category: "G", amount: 19 },
-        { category: "H", amount: 87 },
-      ],
-    };
-  
-    // For an example, see https://github.com/vega/svelte-vega/blob/main/packages/storybook/stories/spec1.ts
-    const spec = VisSpec
-    */
-    import type { VisualizationSpec } from "svelte-vega"
     import  MiserablesData  from "../../data/VisSpec"
     import {default as vegaEmbed} from 'vega-embed';
-    var spec = "https://vega.github.io/vega/examples/force-directed-layout.vg.json";
-
+    // Refer https://www.koderhq.com/tutorial/svelte/json-storage/
     vegaEmbed("#viz", MiserablesData, { actions: false }).catch(error => console.log(error) );
 
   </script>
   
-  <!--
-  <Vega {data} {spec} />
-  -->
   <div id="viz"></div>
 
 <p>Hello Plotsdfas</p>
