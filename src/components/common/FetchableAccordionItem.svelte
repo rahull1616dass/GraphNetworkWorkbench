ede8002f1834aaa45977ad5c41ba921603a4f6fc
Kerem Cömert <kerem0comert@gmail.com>
2022-11-30 19:17:24 +0100

    Develop FromWeb.svelte Accordion

---
+++ src/components/common/FetchableAccordionItem.svelte
@@ -0,0 +1,54 @@
+<!--
+    An accordion that when opened, fetches its content from a URL.
+-->
+<script lang="ts">
+  import { AccordionItem, Button } from "carbon-components-svelte"
+  import request from "../../request"
+  import { createEventDispatcher } from 'svelte'
+
+  export let title: string
+  export let endpoint: string
+  let content = undefined
+
+  const dispatch = createEventDispatcher()
+
+  const onAccordionClick = async (event: MouseEvent) => {
+      if (content == undefined) {
+        content = await request(endpoint)
+      }
+      console.log(content.description)
+  }
+</script>
+
+<AccordionItem
+  on:click={(e) => {
+    onAccordionClick(e)
+  }}
+  bind:title
+>
+  {#if content}
+    <div class="root">
+    <div class="content">
+      <b>Title:</b> {content.title} <br>
+      <b>Description:</b> {content.description}
+    </div>  
+    <div class="import_button">
+    <Button on:click={() => dispatch('fetchNetwork', title)} size="small" on:click>Import Network</Button> 
+  </div>
+  </div>
+  {:else}
+    <p>Fetching content...</p>
+  {/if}
+</AccordionItem>
+
+<style lang="scss">
+  .content {
+    text-align: left;
+    padding: 1rem;
+  }
+
+  .import_button {
+    right: 0;
+    margin: 1rem;
+  }
+</style>
--- src/components/pages/FromWeb.svelte
+++ src/components/pages/FromWeb.svelte
@@ -3,21 +3,49 @@
   import { onMount } from "svelte"
   import { netzschleuderNetworkNames } from "../../stores"
   import { SideNavItems } from "carbon-components-svelte"
+  import FetchableAccordionItem from "../common/FetchableAccordionItem.svelte"
+  import { Accordion } from "carbon-components-svelte"
   import request from "../../request"
 
   onMount(async () => {
     console.log(`${Endpoints.NETZSCHELEUDER}${Endpoints.NETZSCHELEUDER_NETS}`)
     //const response = await request(`${Endpoints.NETZSCHELEUDER}${Endpoints.NETZSCHELEUDER_NETS}`)
-    const response = await request("https://networks.skewed.de/api/nets")
-    netzschleuderNetworkNames.set(response)
+    netzschleuderNetworkNames.set(
+      await request("https://networks.skewed.de/api/nets")
+    )
   })
+
+
+
 </script>
 
 <main>
   <h1>Network Names</h1>
-    {#each $netzschleuderNetworkNames as name}
-      {name}<br>
-    {:else}
-      <p>Geting network names...</p>
-    {/each}
+  {#if $netzschleuderNetworkNames}
+    <Accordion>
+      <div class="networks">
+        {#each $netzschleuderNetworkNames as networkName}
+          <FetchableAccordionItem
+            title={networkName}
+            endpoint={`${Endpoints.NETZSCHELEUDER}${Endpoints.NETZSCLEUDER_NET}${networkName}`}
+            on:fetchNetwork={() => `${Endpoints.NETZSCHELEUDER}/files/${networkName}.csv.zip`}
+          />
+        {/each}
+      </div>
+    </Accordion>
+  {:else}
+    <p>Fetching networks...</p>
+  {/if}
 </main>
+
+<style lang="scss">
+  main {
+    padding: 1rem;
+  }
+
+  .networks {
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
