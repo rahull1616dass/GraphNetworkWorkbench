18543b7bca37762c924829b317e5a4c7a135631c
Kerem Cömert <kerem0comert@gmail.com>
2023-01-09 11:55:11 +0100

    Develop the backend

--- backend/ml/main.py
+++ backend/ml/main.py
@@ -7,27 +7,32 @@ from task_type import TaskType
 app = Flask(__name__)
 
 def response(status: int, data: dict = None) -> Response:
-    return jsonify( {"status": status, **data})
+    data |= {"status": status}
+    print(jsonify(data))
+    return jsonify(data)
 
 def parse_request(request) -> Response:
-    if request.form() is None:
+    if request.json is None:
         return response(400, {"error": "No task type found"})
-    match request.form().get(Fields.TASK_TYPE):
-        case TaskType.NODE_CLASSIFICATION: 
+   
+    match request.json.get(Fields.TASK_TYPE.value):
+        case TaskType.NODE_CLASSIFICATION.value: 
             return response(200, {"message": "Node prediction"})
-        case TaskType.GRAPH_CLASSIFICATION: 
+        case TaskType.GRAPH_CLASSIFICATION.value: 
             return response(200, {"message": "Graph prediction"})
         case _:
             return response(400, {"error": "Invalid task type"})
+    
         
 
 @app.route("/", methods=["GET", "POST"])
 def index() -> Response:
     if request.method == "POST":
         try:
-            print(request.json())
+            print(request.json)
             return parse_request(request)
         except Exception as e:
+            print(e)
             return response(400, {"error": str(e)})
     return response(400, {"error": "Not a POST request"})
 
--- backend/ml/test/test.py
+++ backend/ml/test/test.py
@@ -1,5 +1,9 @@
 import requests
 
-resp = requests.post("https://get-prediction-i5wplx5u5a-nw.a.run.app", files={'file': open('eight.png', 'rb')})
+resp = requests.post(
+    "https://get-prediction-i5wplx5u5a-oa.a.run.app", # http://localhost:5000 ",
+    json={"text": "I love you", "task_type": "node_classification"},
+    headers={"Content-Type": "application/json"},
+)
 
 print(resp.json())
