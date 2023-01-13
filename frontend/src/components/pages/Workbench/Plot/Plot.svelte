71167a5289f7ab1c4aa1c02ad88e8686ab77bd89
Buse <busenuraktilav@gmail.com>
2023-01-13 23:02:48 +0100

    when the network is selected from the dropdown menu, the plot is created -- bug: it does not switch to other pages

--- frontend/src/components/pages/Workbench/Plot/Plot.svelte
+++ frontend/src/components/pages/Workbench/Plot/Plot.svelte
@@ -122,7 +122,34 @@
     })
     loadNetwork()
   }
+
+  export let index: number = undefined
+  let placeholder: string = "Please select a network from the list"
+
+  function selected (index: number) {
+          $selectedNetworkIndex = index
+          loadNetwork()
+          return
+        }
 </script>
+
+
+<div class="dropdown">
+
+  <select class="selectDropdown" bind:value={index} on:click={() => selected(index)} >
+    {#if placeholder}
+    <option >{placeholder}</option>
+    {/if}
+    {#each $networksList as network, index}
+        <option class="optionDropdown" value={index} >
+            {network.metadata.name} ---
+            Nodes: {network.nodes.length} , Edges: {network.links.length} 
+        </option>
+    {/each}
+  </select>
+  
+  
+  </div>
 
 <main>
   <div class="content">
@@ -133,6 +160,7 @@
         </h1>
       </div>
     {:else}
+
       <div class="stats">
         <div class="stats_header">
           <img src={statsIcon} class="stats_icon" alt="Stats Icon" />
@@ -212,4 +240,47 @@
     width: 100%;
     height: 100%;
   }
+
+  .dropdown {
+        position: flex;
+        width: 50%;
+        background-color: whitesmoke;
+        margin-left: 25%;
+        margin-top: 1%;
+    }
+    .selectDropdown {
+        width: 95%;
+        height: 100%;
+        font-family: var(font-family);
+        font-size: 16px;
+        font-weight: 800;
+        color: var(--lightblack);
+        color-scheme: #4a4a56;
+        background-color: white;
+        padding: 1%;
+        margin: 2%;
+        cursor: pointer;
+        transition: background-color 0.2s ease-in-out;
+    }
+    .optionDropdown {
+
+        font-family: var(font-family);
+        font-size: 14px;
+        font-weight: 500;
+        color: var(--lightblack);
+        color-scheme: #4a4a56;
+        background-color: white;
+        cursor: pointer;
+        cursor:hover {
+            background-color: red;
+        }
+    }
+    hr {
+        display: block;
+        margin: 1em;
+        width: 90%;
+        content: "";
+        margin-left: 5%;
+        background-color: whitesmoke;
+    }
 </style>
