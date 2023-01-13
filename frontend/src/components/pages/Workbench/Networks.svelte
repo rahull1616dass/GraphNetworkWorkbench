aa17e8a731c3e1eee3cfbead3970aadf1cf3d9c7
Buse <busenuraktilav@gmail.com>
2023-01-13 23:00:10 +0100

    networks tab is working and redirecting user to the visualize page when a network is selected

--- frontend/src/components/pages/Workbench/Networks.svelte
+++ frontend/src/components/pages/Workbench/Networks.svelte
@@ -1,13 +1,44 @@
 <script lang="ts">
-    import lockIcon from "../../../assets/lock.svg"
-    import rightArrowIcon from "../../../assets/arrow_right.svg"
     import { networksList, selectedNetworkIndex } from "../../../stores";
+    import NetworkListItem from "../../common/NetworkListItem.svelte";
   </script>
   
-  <div class="networks">
-  
+  <div class="explanation-title"> 
+
+    <p> This page shows the networks list you uploaded. Please select a network from the list to visualize its nodes and edged and play with it!</p>  
+    
   </div>
-  
+
+    <div class="networks_list_items" style="--height: {$networksList.length * 120}px;">
+      {#each $networksList as network, index}
+        <NetworkListItem
+          {network}
+          {index}
+          selected={$selectedNetworkIndex == index}
+          on:selectItem={(event) => {
+            $selectedNetworkIndex = event.detail.selectedIndex
+          }}
+        />
+      {/each}
+    </div>
   
   <style lang="scss">
+
+    .networks_list_items {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+    }
+
+    p {
+      font-size: 16px;
+      font-weight: bold;
+      color: var(--lightblack);
+      margin-top: 2%;
+    }
+
   </style>
