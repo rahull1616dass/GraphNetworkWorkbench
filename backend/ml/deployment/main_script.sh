d6b209aa1c6acacdcd2945ef8e0783cef3e5665f
JavaStudentAlex <tarasov.oleksander@gmail.com>
2023-01-24 00:18:40 +0100

    Pushing image to docker registry

--- backend/ml/deployment/main_script.sh
+++ backend/ml/deployment/main_script.sh
@@ -1,1 +1,4 @@
-ssh vingilot -t "buildah bud --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ -t ${BACKEND_IMAGE_TAG} --layers -f ~/xtai_lab3/deployment/Dockerfile;"
+ssh vingilot -t "
+  buildah bud --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ -t ${BACKEND_IMAGE_TAG} --layers -f ~/xtai_lab3/deployment/Dockerfile;
+  buildah push --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ ${BACKEND_IMAGE_TAG}
+  "
