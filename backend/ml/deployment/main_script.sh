477a389d1de7df2f739dca129b82b341e091777d
JavaStudentAlex <tarasov.oleksander@gmail.com>
2023-01-23 23:25:51 +0100

    Fixed env vars in main script

--- backend/ml/deployment/main_script.sh
+++ backend/ml/deployment/main_script.sh
@@ -1,1 +1,1 @@
-ssh vingilot -t "source .bashrc; buildah bud --runroot /tmp/${USER}/.local/share/containers/runroot --root /tmp/${USER}/.local/share/containers/storage/ -t ${XTAI_LAB3_IMAGE_TAG} --layers -f ~/xtai_lab3/deployment/Dockerfile;"
+ssh vingilot -t "buildah bud --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ -t ${BACKEND_IMAGE_TAG} --layers -f ~/xtai_lab3/deployment/Dockerfile;"
