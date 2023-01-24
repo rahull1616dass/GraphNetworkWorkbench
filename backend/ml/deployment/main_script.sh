119f5fa83a8843eee6ed0e36a262f52efd11f53d
JavaStudentAlex <tarasov.oleksander@gmail.com>
2023-01-24 15:54:19 +0100

    Moving to gitlab runner on coder workspace, remove ssh configs when the work is done.

--- backend/ml/deployment/main_script.sh
+++ backend/ml/deployment/main_script.sh
@@ -1,4 +1,5 @@
 ssh vingilot -t "
   buildah bud --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ -t ${BACKEND_IMAGE_TAG} --layers -f ~/xtai_lab3/deployment/Dockerfile;
   buildah push --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ ${BACKEND_IMAGE_TAG};
+  kubectl -n ${VINGILOT_USER} create -f ~/xtai_lab3/deployment/kubernates.yaml
   "
