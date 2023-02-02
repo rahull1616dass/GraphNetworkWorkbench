b08503eef48abbdd9b781198141fd7b5a016446e
JavaStudentAlex <tarasov.oleksander@gmail.com>
2023-02-02 13:03:03 +0100

    Set redirect main route to openapi docs

--- backend/ml/app/api.py
+++ backend/ml/app/api.py
@@ -1,5 +1,6 @@
 """REST API"""
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
 api = APIRouter()
 
@@ -16,17 +17,6 @@ async def node_classification():
     return {"msg": "Node classification is not implemented"}
 
 
-@api.route("/", methods=["GET", "POST"])
-def index() -> FlaskResponse:
-    if request.method == "POST":
-        try:
-            print(request.json)
-            request_json_snake_case = {}
-            # For each key in the request, convert it to snake case
-            for k in request.json.keys(): # type: ignore
-                request_json_snake_case[convert(k)] = request.json[k] # type: ignore
-            return parse_request(MLRequest(**request_json_snake_case))
-        except Exception as e:
-            print(e)
-            return response(400, {"error": str(e)})
-    return response(400, {"error": "Not a POST request"})
+@api.get("/")
+def index():
+    return RedirectResponse("/docs")
