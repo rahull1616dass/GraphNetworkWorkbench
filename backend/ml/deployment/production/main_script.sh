162be0a92e558bf04c69f3d8574f5ca3ce6cc3c8
JavaStudentAlex <tarasov.oleksander@gmail.com>
2023-02-06 17:48:19 +0100

    Fix requirements file not found

--- backend/ml/deployment/production/main_script.sh
+++ backend/ml/deployment/production/main_script.sh
@@ -1,5 +1,5 @@
 ssh vingilot -t "
-  buildah bud --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ -t ${BACKEND_IMAGE_TAG} --layers -f ~/xtai_lab3/deployment/Dockerfile;
+  cd ~xtai_lab3/deployment && buildah bud --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ -t ${BACKEND_IMAGE_TAG} --layers .;
   buildah push --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ ${BACKEND_IMAGE_TAG};
   kubectl -n ${VINGILOT_USER} create -f ~/xtai_lab3/deployment/kubernates.yaml
   "
