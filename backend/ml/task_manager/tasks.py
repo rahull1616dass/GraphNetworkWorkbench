0ddba758306f899ab85abf17730f263229090b1e
JavaStudentAlex <tarasov.oleksander@gmail.com>
2023-02-07 09:11:25 +0100

    Move celery instance to init package file and add it also to the fastapi instance

--- backend/ml/task_manager/tasks.py
+++ backend/ml/task_manager/tasks.py
@@ -1,43 +1,27 @@
 """Celery tasks"""
-from celery import Celery
+from celery import shared_task
 
-from app.config import Settings
+from task_manager import celery_instance
 from ml.link_prediction import predict_edges
 from ml.node_classifier import classify_nodes
 from preprocessing.parsing import from_dataframe
 from preprocessing.loading import download_network_files
 
-settings = Settings()
-
-celery_queue = Celery(
-    __name__,
-    broker=f"amqp://{settings.rabbitmq_host}:{settings.rabbitmq_port}",
-    backend=f"redis://{settings.redis_host}:{settings.redis_port}"
-)
-
-celery_queue.conf.update(
-    accept_content={"pickle"},
-    task_serializer="pickle",
-    result_serializer="pickle",
-    event_serializer="pickle"
-)
-
-
-@celery_queue.task
+@shared_task
 def download_task(*args, **kwargs):
     return download_network_files(*args, **kwargs)
 
 
-@celery_queue.task
+@shared_task
 def convert_task(*args, **kwargs):
     return from_dataframe(*args, **kwargs)
 
 
-@celery_queue.task
+@shared_task
 def edge_prediction_task(*args, **kwargs):
     return predict_edges(*args, **kwargs)
 
 
-@celery_queue.task
+@shared_task
 def node_classification_task(*args, **kwargs):
     return classify_nodes(*args, **kwargs)
