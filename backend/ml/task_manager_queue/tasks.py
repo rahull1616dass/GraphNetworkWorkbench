2a6e2ca708084a9048c0e1d2e675e1f542fa0bd7
Alex Tarasov <tarasik201617@gmail.com>
2023-02-07 08:27:31 +0100

    Add config file for env vars

--- backend/ml/task_manager_queue/tasks.py
+++ backend/ml/task_manager_queue/tasks.py
@@ -1,15 +1,18 @@
 """Celery tasks"""
 from celery import Celery
 
+from app.config import Settings
 from ml.link_prediction import predict_edges
 from ml.node_classifier import classify_nodes
 from preprocessing.parsing import from_dataframe
 from preprocessing.loading import download_network_files
 
+settings = Settings()
+
 celery_queue = Celery(
     __name__,
-    broker="amqp://localhost:5672",
-    backend="redis://localhost:6379"
+    broker=f"amqp://{settings.rabbitmq_host}:{settings.rabbitmq_port}",
+    backend=f"redis://{settings.redis_host}:{settings.redis_port}"
 )
 
 celery_queue.conf.update(
@@ -19,13 +22,16 @@ celery_queue.conf.update(
     event_serializer="pickle"
 )
 
+
 @celery_queue.task
 def download_task(*args, **kwargs):
     return download_network_files(*args, **kwargs)
 
+
 @celery_queue.task
 def convert_task(*args, **kwargs):
     return from_dataframe(*args, **kwargs)
+
 
 @celery_queue.task
 def edge_prediction_task(*args, **kwargs):
