e96fcc9e08f473a5953fc2788763ffa9cff62db6
JavaStudentAlex <tarasov.oleksander@gmail.com>
2023-02-17 18:51:26 +0100

    Change the way the application is deployed.

--- backend/ml/deployment/production/main_script.sh
+++ backend/ml/deployment/production/main_script.sh
@@ -1,5 +1,9 @@
 ssh vingilot -t "
-  cd ./xtai_lab3/deployment && buildah bud --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ -t ${BACKEND_IMAGE_TAG} --layers .;
+  cd ~/xtai_lab3/deployment.new && buildah bud --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ -t ${BACKEND_IMAGE_TAG} --layers .;
   buildah push --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ ${BACKEND_IMAGE_TAG};
+  if test -f ~/xtai_lab3/deployment/kubernates.yaml; then kubectl -n ${VINGILOT_USER} delete ~/xtai_lab3/deployment/kubernates.yaml --all; rm -rf ~/xtai_lab3/deployment; fi;
+  mv ~/xtai_lab3/deployment.new ~/xtai_lab3/deployment
+  if test -d ./xtai_lab3/application; then rm -rf ./xtai_lab3/application; fi;
+  mv ./xtai_lab3/application.new ./xtai_lab3/application
   kubectl -n ${VINGILOT_USER} create -f ~/xtai_lab3/deployment/kubernates.yaml
-  "
+"
