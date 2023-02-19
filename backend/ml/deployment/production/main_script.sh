d10ed413e23a74614e3aada039d9d35d9e2c728e
Alex Tarasov <tarasik201617@gmail.com>
2023-02-19 01:30:39 +0100

    Test deployment.

--- backend/ml/deployment/production/main_script.sh
+++ backend/ml/deployment/production/main_script.sh
@@ -4,6 +4,6 @@ ssh vingilot -t "
   if test -f /home/stud/tarasov/xtai_lab3/deployment/kubernates.yaml; then kubectl -n ${VINGILOT_USER} delete -f /home/stud/tarasov/xtai_lab3/deployment/kubernates.yaml --all; rm -rf /home/stud/tarasov/xtai_lab3/deployment; fi;
   mv /home/stud/tarasov/xtai_lab3/deployment.new /home/stud/tarasov/xtai_lab3/deployment
   if test -d /home/stud/tarasov/xtai_lab3/application; then rm -rf /home/stud/tarasov/xtai_lab3/application; fi;
-  mv /home/stud/tarasov/xtai_lab3/application.new /home/stud/tarasov/xtai_lab3/application
-  kubectl -n ${VINGILOT_USER} create -f /home/stud/tarasov/xtai_lab3/deployment/kubernates.yaml
+  mv /home/stud/tarasov/xtai_lab3/application.new /home/stud/tarasov/xtai_lab3/application;
+  kubectl -n ${VINGILOT_USER} create -f /home/stud/tarasov/xtai_lab3/deployment/kubernates.yaml;
 "
