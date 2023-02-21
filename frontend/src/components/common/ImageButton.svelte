7ba0b69ae76713c653fc115cff46ae2b878f661e
Rahul <rahull1616dass@gmail.com>
2023-02-21 19:36:49 +0100

    Updated with delete button fix

--- frontend/src/components/common/ImageButton.svelte
+++ frontend/src/components/common/ImageButton.svelte
@@ -1,20 +1,27 @@
 <script>
 
-export let defaultImageSource;
-
+export let defaultImageSource
+export let styleClass = "defaultStyle"
 </script>
 
-<button on:click>
+<button on:click class={styleClass} >
     <img src= {defaultImageSource}>
     <slot></slot>
 </button>
 
 
 <style lang="scss">
-    button {
+    .defaultStyle {
       position: relative;
       top: -160px;
       right: 60px;
+      border: none;
+      color: transparent;
+      padding: 0px;
+      background-color: transparent;
+    }
+    .deleteButtonClass{
+      position: relative;
       border: none;
       color: transparent;
       padding: 0px;
