09a26f25e1e1b756240f84f3b72c4ba4b5a44f58
JavaStudentAlex <tarasov.oleksander@gmail.com>
2023-02-23 15:11:24 +0100

    Change the way graphs are exported to data

--- backend/ml/preprocessing/parsing.py
+++ backend/ml/preprocessing/parsing.py
@@ -1,35 +1,23 @@
 from typing import Dict
 
-import torch
-import numpy as np
 import pandas as pd
-from math import ceil
-from torch_geometric.data import Data as TorchGeoData
+import networkx as nx
+from torch_geometric.data import Data
+from torch_geometric.utils.convert import from_networkx
 
 from core.types import MLTask
 from core.logging_helpers import timeit
 
 
-def generate_train_mask(dataset_size: int, train_percentage: float) -> torch.Tensor:
-    train_count: int = ceil(train_percentage * dataset_size)
-    test_count: int = dataset_size - train_count
-    train_mask: torch.Tensor = torch.tensor([True] * train_count + [False] * test_count)
-
-    # Randomly shuffle the train_mask
-    return train_mask[torch.randperm(train_mask.size(0))]
-
-
 @timeit
-def from_dataframe(data: Dict[str, pd.DataFrame], task: MLTask) -> TorchGeoData:
-    nodes = data["nodes"]
-    edges = data["edges"]
-    y: np.ndarray = nodes["groups"].to_numpy()
-    train_mask: torch.Tensor = generate_train_mask(y.shape[0], task.train_percentage)
+def to_graph_data(data_files: Dict[str, str], task: MLTask) -> Data:
+    nodes = pd.read_csv(data_files["nodes"], index_col="index")
+    edges = pd.read_csv(data_files["edges"])
 
-    return TorchGeoData(
-        x=torch.from_numpy(np.eye(nodes.shape[0])).to(torch.float32),
-        y=torch.from_numpy(nodes["groups"].to_numpy()),
-        edge_index=torch.Tensor(edges.to_numpy()).t().contiguous().to(torch.int64),
-        train_mask=train_mask,
-        test_mask=~train_mask
-    )
+    graph = nx.DiGraph()
+    graph.add_edges_from(list(edges.itertuples(index=False)))
+
+    features = list(set(nodes.columns) - {"name"})
+    list(map(lambda column_name: nx.set_node_attributes(graph, nodes[column_name].to_dict(), column_name), features))
+
+    return from_networkx(graph, features)
