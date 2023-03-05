3b9bf0287d6cbd3b41ac34b048f0ae523325af4f
JavaStudentAlex <tarasov.oleksander@gmail.com>
2023-03-05 20:33:20 +0100

    Added expert opinion with chatgpt

--- backend/ml/app/api.py
+++ backend/ml/app/api.py
@@ -1,4 +1,5 @@
 """REST API"""
+from celery import chain
 from fastapi import APIRouter, Body
 from fastapi.responses import RedirectResponse, FileResponse
 
@@ -7,22 +8,25 @@ from app.workflows import (
     link_prediction_workflow,
     node_classification_workflow
 )
-from app.expertise import get_expert_advice_about_class
+from app.workflows import run_workflow
 from core.requests import MLRequest, ClassificationRequest
 from core.responses import NodeClassResponse, EdgePredResponse
+from task_manager.tasks import get_class_expert_opinion_task, get_pred_expert_opinion_task
 
 api = APIRouter()
 logger = get_logger(__name__)
 
 
 @api.post("/link_pred")
-async def link_prediction(task: MLRequest = Body()):
-    losses, val_scores, test_score, predictions =  await link_prediction_workflow(task)
+async def link_prediction(request: MLRequest = Body()):
+    losses, val_scores, test_score, predictions =  await link_prediction_workflow(request)
+    expert_opinion = await run_workflow(chain(get_pred_expert_opinion_task.s(request, losses, val_scores, test_score)))
     response = EdgePredResponse(
         losses=losses,
         score=test_score,
         validation_scores=val_scores,
-        predictions=predictions
+        predictions=predictions,
+        expert_opinion=expert_opinion
     )
     return response
 
@@ -30,7 +34,7 @@ async def link_prediction(task: MLRequest = Body()):
 @api.post("/node_class")
 async def node_classification(request: ClassificationRequest = Body()):
     losses, val_acc_scores, accuracy, predictions =  await node_classification_workflow(request)
-    expert_opinion = await get_expert_advice_about_class(request, losses, val_acc_scores, accuracy)
+    expert_opinion = await run_workflow(chain(get_class_expert_opinion_task.s(request, losses, val_acc_scores, accuracy)))
     response = NodeClassResponse(
         losses=losses,
         predictions=predictions,
---
+++ backend/ml/chatgpt/expertise.py
@@ -0,0 +1,63 @@
+from typing import List, Tuple
+
+import openai
+
+from chatgpt import settings
+from core.loggers import timeit, get_logger
+from core.requests import MLRequest, ClassificationRequest
+
+logger = get_logger(__file__)
+
+
+def get_basic_arguments(request: MLRequest, losses: List[float], validation_scores: List[float])-> Tuple:
+    return (
+        len(request.hidden_layer_sizes),
+        request.ml_model_type,
+        "[" + ", ".join(map(str, request.hidden_layer_sizes)) + "]",
+        len(request.x_columns),
+        request.train_percentage,
+        1 - request.train_percentage,
+        request.learning_rate,
+        request.epochs,
+        "[" + ", ".join(map(str, losses)) + "]",
+        "[" + ", ".join(map(str, validation_scores)) + "]"
+    )
+
+
+@timeit
+async def get_expert_advice_about_class(
+        request: ClassificationRequest, losses: List[float], validation_scores: List[float], test_score: float):
+    layers, model, layers_sizes, features, train, val, lr, epochs, losses_str, vals_str = \
+        get_basic_arguments(request, losses, validation_scores)
+    prompt = f"We have trained a graph neural network for node classification task by using " \
+             f"{layers} hidden layers with {layers_sizes} layers sizes with {model} model. After each layer the " \
+             f"Relu activation function was used and the output layer has the LogSoftmax activation function. " \
+             f"We have used {features} nodes features for it. The data set was divided into train and validation " \
+             f"datasets with {train} and {val} frequencies respectively. During the training we have used negative " \
+             f"log likelihood loss function and ADAM optimizer with the learning rate {lr}. We trained model during " \
+             f"{epochs} epochs. We got such list of losses {losses_str} and such list of validation accuracy scores " \
+             f"{vals_str}. The final model accuracy is {test_score}. How to optimize the model?"
+    response = await openai.Completion.acreate(engine=settings.expert_model, prompt=prompt, max_tokens=200)
+    return response.choices[0].text
+
+
+@timeit
+async def get_expert_advice_about_pred(
+        request: MLRequest, losses: List[float], roc_auc_val_scores: List[float], roc_auc_test_score: float):
+    layers, model, layers_sizes, features, train, val, lr, epochs, losses_str, vals_str = \
+        get_basic_arguments(request, losses, roc_auc_val_scores)
+    prompt = f"We have trained a graph neural network for edge prediction task by using " \
+             f"{layers} hidden layers with {layers_sizes} layers sizes with {model} model. We have used " \
+             f"{features} nodes features for it. The data set was divided into train and validation datasets with " \
+             f"{train} and {val} frequencies respectively. The model described before was using in encoding part. " \
+             f"Each iteration we first calculated the encodings, then the negative sampling had place and finally " \
+             f"the decoding part was done on both positive and negative edge indexes. The BCEWithLogitsLoss was used " \
+             f"as loss function and ADAM with the learning rate {lr} was used as optimizer. We trained model during " \
+             f"{epochs} epochs. We got such list of losses {losses_str} and such list of test ROC AUC scores " \
+             f"{vals_str}. The final model ROC AUC score is {roc_auc_test_score}. Please advice to optimize the model" \
+             f"without writing code snippets?"
+    logger.info(prompt)
+    response = await openai.Completion.acreate(engine=settings.expert_model, prompt=prompt, max_tokens=200)
+    text = response.choices[0].text
+    logger.info(text)
+    return response.choices[0].text
