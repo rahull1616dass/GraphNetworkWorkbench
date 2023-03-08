1b6620fbc433cb1e277ace8007addb4cb97ea92c
JavaStudentAlex <tarasov.oleksander@gmail.com>
2023-03-08 18:05:43 +0100

    Fix creating kubernates pods from one config file

--- backend/ml/deployment/production/main_script.sh
+++ backend/ml/deployment/production/main_script.sh
@@ -1,7 +1,7 @@
 ssh vingilot -t "
   cd /home/stud/tarasov/xtai_lab3/deployment.new && buildah bud --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ -t ${BACKEND_IMAGE_TAG} --layers . && cd /home/stud/tarasov;
   buildah push --runroot /tmp/${VINGILOT_USER}/.local/share/containers/runroot --root /tmp/${VINGILOT_USER}/.local/share/containers/storage/ ${BACKEND_IMAGE_TAG};
-  if test -d /home/stud/tarasov/xtai_lab3/deployment/kubernates; then kubectl -n ${VINGILOT_USER} delete -f /home/stud/tarasov/xtai_lab3/deployment/kubernates -R --all; rm -rf /home/stud/tarasov/xtai_lab3/deployment; fi;
+  if test -d /home/stud/tarasov/xtai_lab3/deployment/kubernates; then kubectl -n ${VINGILOT_USER} delete -f /home/stud/tarasov/xtai_lab3/deployment/kubernates/ -R --all; rm -rf /home/stud/tarasov/xtai_lab3/deployment; fi;
   mv /home/stud/tarasov/xtai_lab3/deployment.new /home/stud/tarasov/xtai_lab3/deployment;
   if test -d /home/stud/tarasov/xtai_lab3/application; then rm -rf /home/stud/tarasov/xtai_lab3/application; fi;
   mv /home/stud/tarasov/xtai_lab3/application.new /home/stud/tarasov/xtai_lab3/application;
@@ -11,5 +11,5 @@ ssh vingilot -t "
   EXPERT_MODEL=${EXPERT_MODEL}
   ' > /home/stud/tarasov/xtai_lab3/application/.env;
   mkdir -p /home/stud/tarasov/xtai_lab3/redis_data;
-  kubectl -n ${VINGILOT_USER} create -f /home/stud/tarasov/xtai_lab3/deployment/kubernates.yaml;
+  kubectl -n ${VINGILOT_USER} create -f /home/stud/tarasov/xtai_lab3/deployment/kubernates/ -R;
 "
